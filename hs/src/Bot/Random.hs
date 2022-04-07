29190252f19f538582cbb8466a4e9ffe0831b737
Julien Dehos <dehos@univ-littoral.fr>
2022-04-07 23:08:15 +0200

    find5

---
+++ hs/src/Bot/Random.hs
@@ -0,0 +1,2 @@
+module Bot.Random where
+
---
+++ hs/src/Game.hs
@@ -0,0 +1,25 @@
+{-# Language FunctionalDependencies #-}
+
+module Game where
+
+import qualified Data.Vector as V
+
+class Game g a s o | g -> a s o where
+  isRunning :: g a s o -> Bool
+  isTerminated :: g a s o -> Bool
+  getStatus :: g a s o -> s
+  getObservations :: g a s o -> V.Vector o
+  getActions :: g a s o -> V.Vector a
+  playAction :: Eq a => a -> g a s o -> g a s o
+
+  isTerminated = not . isRunning
+
+  {-
+  playIndex :: Int -> g a s o -> g a s o
+
+  playAction a g =
+      case V.findIndex (==a) (getActions g) of
+          Nothing -> g
+          Just n -> playIndex n g
+  -}
+
