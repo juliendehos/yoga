14c1e29bcb6a3a54eec46dad6f68609e5454722d
Julien Dehos <dehos@univ-littoral.fr>
2022-04-18 00:46:12 +0200

    hs step wip

--- hs/app/console/Main.hs
+++ hs/app/console/Main.hs
@@ -1,4 +1,5 @@
 
+import Control.Concurrent
 import Control.Monad.ST
 import qualified Data.Massiv.Array as M
 import System.Random.MWC
@@ -14,28 +15,30 @@ formatCell Cat = "C"
 
 showEnv :: Env s -> ST s String
 showEnv env = do
-  b <- M.freezeS (_board env)
+  b <- M.freezeS (_eBoard env)
   return $ unlines $ map (concatMap formatCell) $ M.toLists2 b
 
 run :: Env RealWorld -> Int -> IO (Env RealWorld)
 run env0 nSims =
   let go :: Env RealWorld -> Int -> Int -> IO (Env RealWorld)
       go env iSim iStep
-        | _done env = do
+        | _eDone env = do
               env1 <- stToIO $ reset env
               go env1 (iSim+1) 0
         | iSim >= nSims = return env
         | otherwise = do
+            threadDelay 200000
             putStrLn $ "iSim: " <> show iSim
             putStrLn $ "iStep: " <> show iStep
             stToIO (showEnv env) >>= putStrLn
-            go (step Yoga.Left env) iSim (iStep+1)
+            env1 <- stToIO $ step Yoga.Left env
+            go env1 iSim (iStep+1)
   in go env0 0 0
 
 main :: IO ()
 main = do
   gen <- createSystemRandom
-  env <- stToIO $ mkEnv 15 30 30 gen
+  env <- stToIO (mkEnv 15 30 30 gen >>= reset)
   _ <- run env 3
   return ()
 
--- hs/src/Yoga/Env.hs
+++ hs/src/Yoga/Env.hs
@@ -1,4 +1,5 @@
 {-# Language StrictData #-}
+{-# Language TemplateHaskell #-}
 
 module Yoga.Env 
   ( Env (..)
@@ -12,8 +13,8 @@ module Yoga.Env
   ) where
  
 import Control.Monad.ST
-import Data.Massiv.Array as M
--- import Data.List (foldl')
+import Control.Lens hiding (Empty)
+import Data.Massiv.Array as M hiding (mapM)
 import System.Random.MWC
 
 data Action = Left | Front | Right
@@ -34,22 +35,42 @@ data Cell = Empty | Cat | Dog | Food | Wall
 type Board s = MArray s B Ix2 Cell
 
 data Env s = Env
-  { _score :: Double
-  , _done :: Bool
-  , _actionSpace :: ActionSpace
-  , _observationSpace :: ObservationSpace
-  , _observation :: Observation
-  , _startingVitality :: Double
-  , _itemCapacity :: Int
-  , _catPij :: Ix2
-  , _catDij :: Ix2
-  , _board :: Board s
-  , _lastAction :: Maybe Action
-  , _vitality :: Double
-  , _foods :: [Ix2]
-  , _dogs :: [Ix2]
-  , _gen :: GenST s
+  { _eScore :: Double
+  , _eDone :: Bool
+  , _eActionSpace :: ActionSpace
+  , _eObservationSpace :: ObservationSpace
+  , _eObservation :: Observation
+  , _eStartingVitality :: Double
+  , _eItemCapacity :: Int
+  , _eCatPij :: Ix2
+  , _eCatDij :: Ix2
+  , _eBoard :: Board s
+  , _eLastAction :: Maybe Action
+  , _eVitality :: Double
+  , _eFoods :: [Ix2]
+  , _eDogs :: [Ix2]
+  , _eGen :: GenST s
   }
+makeLenses ''Env
+
+mkEnv :: Int -> Int -> Double -> GenST s -> ST s (Env s)
+mkEnv ni0 nj0 sVitality gen = do
+  let ni = ni0 + 2
+      nj = nj0 + 2
+  board <- newMArray (Sz2 ni nj) Empty
+  reset' board sVitality gen
+
+reset :: Env s -> ST s (Env s)
+reset env = reset' (_eBoard env) (_eStartingVitality env) (_eGen env)
+
+step :: Action -> Env s -> ST s (Env s)
+step _a env = do
+  let ij = env^.eCatPij
+  write_ (env^.eBoard) ij Cat
+  return $ env
+      & eDone .~ (env^.eScore > 4)
+      & eScore +~ 1
+      & eCatPij +~ (env^.eCatDij)
 
 -- maxScore :: Double
 -- maxScore = 100
@@ -60,63 +81,59 @@ mkObservation _board _pij _dij vitality =
   let left = Empty
   in Observation left Empty [] vitality
 
-mkBoard :: Int -> Int -> ST s (Board s)
-mkBoard ni nj = do
-  let fBoard (Ix2 i j) = 
-        return (if i==0 || j==0 || i==(ni-1) || j==(nj-1) then Wall else Empty)
-  makeMArrayS (Sz2 ni nj) fBoard
+resetBoard :: Board s -> ST s ()
+resetBoard board =
+  let (Sz2 ni nj) = msize board
+  in iforPrimM_ board $ \ij@(Ix2 i j) _ -> 
+        write_ board ij 
+          (if i==0 || j==0 || i==(ni-1) || j==(nj-1) then Wall else Empty)
 
-addItems :: Board s -> GenST s -> Int -> Cell -> ST s [Ix2]
-addItems board gen _nb cell = do
+addCat :: Board s -> GenST s -> ST s Ix2
+addCat board gen = do
   let (Sz2 ni nj) = msize board
   i <- (+ ni `div` 2) <$> uniformR (-2, 2) gen
   j <- (+ nj `div` 2) <$> uniformR (-2, 2) gen
-  write_ board (Ix2 i j) cell
-  return [Ix2 i j]
+  write_ board (Ix2 i j) Cat
+  return (Ix2 i j)
 
-{-
-addItems :: Board -> StdGen -> Int -> Cell -> (Board, StdGen, [Ix2])
-addItems board gen nb cell = 
-  let sample b g = (b, g, Ix2 1 1)
-      addItem (b, g, xs) _ =
-        let (b1, g1, x1) = sample b g
-            b2 = 
-        in (b1, g1, x1:xs)
-  in foldl' addItem (board, gen, []) [1..nb]
--}
+addItems :: Board s -> GenST s -> Int -> Cell -> ST s [Ix2]
+addItems board gen nb cell = do
+  let (Sz2 ni nj) = msize board
+      fSample n = do
+        i <- uniformR (1, ni-2) gen
+        j <- uniformR (1, nj-2) gen
+        let ij = Ix2 i j
+        c <- readM board ij
+        case c of
+          Empty -> write_ board ij cell >> return ij
+          _ -> fSample n
+  mapM fSample [1..nb] 
 
-
-
-mkEnv :: Int -> Int -> Double -> GenST s -> ST s (Env s)
-mkEnv ni0 nj0 sVitality gen = do
-  let ni = ni0 + 2
-      nj = nj0 + 2
+reset' :: Board s -> Double -> GenST s -> ST s (Env s)
+reset' board sVitality gen = do
+  let (Sz2 ni nj) = msize board
       dij = Ix2 (-1) 0
-  board <- mkBoard ni nj
-  [pij] <- addItems board gen 1 Cat
+      itemCapacity = (ni + nj) `div` 2
+      itemCapacity0 = itemCapacity `div` 2
+  resetBoard board
+  pij <- addCat board gen
+  foods <- addItems board gen itemCapacity0 Food
+  dogs <- addItems board gen itemCapacity0 Dog
   return Env
-      { _score = 0
-      , _done = False
-      , _actionSpace = ActionSpace
-      , _observationSpace = ObservationSpace
-      , _observation = mkObservation board pij dij sVitality 
-      , _startingVitality = sVitality
-      , _itemCapacity = (ni + nj) `div` 2
-      , _catPij = pij
-      , _catDij = dij
-      , _board = board
-      , _lastAction = Nothing
-      , _vitality = sVitality
-      , _foods = []
-      , _dogs = []
-      , _gen = gen
+      { _eScore = 0
+      , _eDone = False
+      , _eActionSpace = ActionSpace
+      , _eObservationSpace = ObservationSpace
+      , _eObservation = mkObservation board pij dij sVitality 
+      , _eStartingVitality = sVitality
+      , _eItemCapacity = itemCapacity0
+      , _eCatPij = pij
+      , _eCatDij = dij
+      , _eBoard = board
+      , _eLastAction = Nothing
+      , _eVitality = sVitality
+      , _eFoods = foods
+      , _eDogs = dogs
+      , _eGen = gen
       }
-
-reset :: Env s -> ST s (Env s)
-reset env = 
-  let (Sz2 ni nj) = msize (_board env)  -- sizeOfMArray 
-  in mkEnv (ni-2) (nj-2) (_startingVitality env) (_gen env)
-
-step :: Action -> Env s -> Env s
-step _a e = e { _done = True }
 
