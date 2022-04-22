8af7c67b19a37f79674f17c41506aadf171a5636
Julien Dehos <dehos@univ-littoral.fr>
2022-04-22 16:19:29 +0200

    hs test v4

--- hs/app/test/Main.hs
+++ hs/app/test/Main.hs
@@ -14,44 +14,44 @@ import Control.Monad.ST.Class
 -- env + agent
 -------------------------------------------------------------------------------
 
-class MonadEnv env actionSpace action m | env -> actionSpace action where
-  getActionSpace :: env -> m actionSpace
+class MonadEnv env space action m | env -> space action where
+  getActionSpace :: env -> m space
   step :: action -> env -> m env
 
-class MonadAgent actionSpace action agent m where
-  genAction :: actionSpace -> agent -> m (action, agent)
+class MonadAgent space action agent m where
+  genAction :: space -> agent -> m (action, agent)
 
 -------------------------------------------------------------------------------
 -- citycat
 -------------------------------------------------------------------------------
 
-newtype ActionSpace = ActionSpace { unActionSpace :: [Action] }
+data ActionCitycat = ACLeft | ACFront | ACRight
   deriving Show
 
-data Action = ALeft | AFront | ARight
+newtype SpaceCitycat = SpaceCitycat { unSpace :: [ActionCitycat] }
   deriving Show
 
 data Citycat = Citycat
-  { _cLastAction :: Maybe Action
+  { _cLastAction :: Maybe ActionCitycat
   , _cSteps :: Int
   } deriving Show
 
 mkCitycat :: Citycat
 mkCitycat = Citycat Nothing 0
 
-stepCitycat :: Action -> Citycat -> Citycat
+stepCitycat :: ActionCitycat -> Citycat -> Citycat
 stepCitycat action citycat = citycat 
   { _cLastAction = Just action
   , _cSteps = 1 + _cSteps citycat
   }
 
-getActionSpaceCitycat :: ActionSpace
-getActionSpaceCitycat = ActionSpace [ALeft, AFront, ARight]
+getActionSpaceCitycat :: SpaceCitycat
+getActionSpaceCitycat = SpaceCitycat [ACLeft, ACFront, ACRight]
 
 newtype EnvCitycat m a = EnvCitycat { runEnvCitycat :: m a }
-  deriving (Functor, Applicative, Monad, MonadST, MonadIO)
+  deriving (Functor, Applicative, Monad, MonadST, MonadIO, MonadRandom)
 
-instance Monad m => MonadEnv Citycat ActionSpace Action (EnvCitycat m) where
+instance Monad m => MonadEnv Citycat SpaceCitycat ActionCitycat (EnvCitycat m) where
   getActionSpace _env = return getActionSpaceCitycat
 
   step action env = return $ stepCitycat action env
@@ -68,19 +68,42 @@ mkExpert :: Expert
 mkExpert = Expert 0
 
 newtype AgentExpert m a = AgentExpert { runAgentExpert :: m a }
-  deriving (Functor, Applicative, Monad, MonadIO, MonadEnv env actionSpace action)
+  deriving (Functor, Applicative, Monad, MonadIO, MonadEnv env space action)
 
-instance (MonadEnv Citycat ActionSpace Action m, Monad m) 
-  => MonadAgent ActionSpace Action Expert (AgentExpert m) 
+instance (MonadEnv Citycat SpaceCitycat ActionCitycat m, Monad m) 
+  => MonadAgent SpaceCitycat ActionCitycat Expert (AgentExpert m) 
   where
 
     genAction space0 agent0 = 
-      let actions = unActionSpace space0
+      let actions = unSpace space0
           i1 = _eIndex agent0 + 1
           i2 = if i1 < length actions then i1 else 0
           action = actions !! i2
           agent1 = agent0 { _eIndex = i2 }
       in return (action, agent1)
+
+----------------------------------------------------------------
+-- random
+-------------------------------------------------------------------------------
+
+data Random = Random
+  deriving Show
+
+mkRandom :: Random
+mkRandom = Random
+
+newtype AgentRandom m a = AgentRandom { runAgentRandom :: m a }
+  deriving (Functor, Applicative, Monad, MonadIO, MonadEnv env space action, MonadRandom)
+
+instance (MonadEnv Citycat SpaceCitycat ActionCitycat m, MonadRandom m) 
+  => MonadAgent SpaceCitycat ActionCitycat Random (AgentRandom m) 
+  where
+
+    genAction space0 agent0 = do
+      let actions = unSpace space0
+      i <- getRandomR (0, length actions - 1)
+      let action = actions !! i
+      return (action, agent0)
 
 -------------------------------------------------------------------------------
 -- app + main
@@ -101,11 +124,12 @@ app env0 agent0 = do
 
 main :: IO ()
 main = do
-  putStrLn "test"
   let 
     env0 = mkCitycat
     agent0 = mkExpert
+    -- agent0 = mkRandom
   (env1, agent1) <- runEnvCitycat $ runAgentExpert $ app env0 agent0
+  -- (env1, agent1) <- runEnvCitycat $ runAgentRandom $ app env0 agent0
   print env1
   print agent1
 
